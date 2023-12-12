ccf529b001c55541bb320c64af78cc1aac5a9276
AtyraNazar <94417099+Nazarkulova@users.noreply.github.com>
2023-12-11 23:17:44 -0500

    Update settings.py

--- Blog-Project/src/cblog/settings.py
+++ Blog-Project/src/cblog/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database1', # database name in RDS is written here
+        'NAME': 'RDSConfig', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
-        'PASSWORD': 'Tech1234',
-        'HOST': 'database1.cewvxnnnahzx.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'PASSWORD': 'At221090',
+        'HOST': 'database-1.c8lnubljlp6w.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
@@ -144,8 +144,8 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = 'blog-s3-techtorial' # please enter your s3 bucket name
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_STORAGE_BUCKET_NAME = 'project-atyrabucket' # please enter your s3 bucket name
+AWS_S3_CUSTOM_DOMAIN = 'static.atyratechtorial.link' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
 
